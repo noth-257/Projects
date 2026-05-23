97f49380c7dbcf7d0886bf4b30973461deaa67f3
Abhishek Pratap Singh <pratapabhishek257@gmail.com>
2026-05-24 02:40:20 +0530

    merged all branches

--- src/components/reader/HighlightPopup.jsx
+++ src/components/reader/HighlightPopup.jsx
@@ -29,7 +29,6 @@ export default function HighlightPopup({ position, onSelect, onClose, onFormat, 
   // Fix 4: flip popup below text if near top of screen
   const spaceAbove = position.y;
   const showBelow  = spaceAbove < 500;
-  const showBelow  = spaceAbove <200;
   const topPos     = showBelow ? position.y + 30 : position.y - 60;
 
   return (
